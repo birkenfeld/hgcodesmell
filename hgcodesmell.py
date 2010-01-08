e7639a2bd3d6a559343544d577ea008d80be9e60
Georg Brandl <georg@python.org>
2010-01-08 14:57:41 +0100

    Change terminology a bit.

--- hgcodesmell.py
+++ hgcodesmell.py
@@ -41,14 +41,14 @@ try:
 except ImportError:
     colorwrap = lambda o, s: o(s)
 
-# smelly things are tuples (regex, reason)
+# smelly patterns are tuples (regex, reason)
 print_stmt = (re.compile(r'^\+\s*print\b'), 'print statement')
 zero_div = (re.compile(r'^\+\s*1/0'), 'zero division error')
 set_trace = (re.compile(r'\bpdb\.set_trace\(\)'), 'set_trace')
 vim_cmd = (re.compile(r':(w|wq|q|x)$', re.M), 'vim exit command')
 windows_nl = (re.compile(r'\r'), 'Windows newline')
 
-# maps glob patterns to a list of smelly things
+# the master dict maps glob patterns to a list of smelly patterns
 SMELLY_STUFF = {
     '*.py': [print_stmt, zero_div, set_trace],
     '*': [vim_cmd],
