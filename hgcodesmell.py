917196eeae516e9d9536b106aa767a55b94c5ea4
Georg Brandl <georg@python.org>
2010-09-02 10:43:54 +0200

    Adapt docstrings to Mercurial customs.

--- hgcodesmell.py
+++ hgcodesmell.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
-"""
-    hgcodesmell
-    ~~~~~~~~~~~
+#
+# codesmell mercurial extension
+#
+# Copyright (c) 2009-2010 by Georg Brandl.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+"""warn about smelly changes in added code before allowing a commit
 
-    Mercurial extension to warn about smelly changes in added code
-    before allowing a commit.
+Usage: activate the extension in your hgrc file::
 
-    Usage: activate the extension in your hgrc file::
+    [extensions]
+    hgcodesmell = path/to/hgcodesmell.py
 
-        [extensions]
-        hgcodesmell = path/to/hgcodesmell.py
-
-    :copyright: 2009, 2010 by Georg Brandl.
-    :license:
-        This program is free software; you can redistribute it and/or
-        modify it under the terms of the GNU General Public License as
-        published by the Free Software Foundation; either version 2 of
-        the License, or (at your option) any later version.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public
-        License along with this program; if not, write to the Free
-        Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-        Boston, MA 02110-1301 USA.
+Then, when "smelly" changes would be committed, they will be printed out and you
+will be prompted whether to actually do the commit.
 """
 
 import os
